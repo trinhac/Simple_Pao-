fee9c104f2d06a700f8b64960519a1b856b9a322
trinhac <cuong.ta14584@sinhvien.hoasen.edu.vn>
2022-08-05 23:07:54 +0700

    Web22A's Homework Day 4 - April 3 2022: Simple Online News  Using Angular,Nebular Theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { NbMenuItem, NbThemeService } from '@nebular/theme';
+import { NbSidebarService } from '@nebular/theme';
+import { Router } from '@angular/router';
+import { GetArticlesService } from './services/article/get-articles.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
+
   title = 'TheNews';
+  constructor(private sidebarService: NbSidebarService, private router:Router, private themeService: NbThemeService) {}
+
+  ngOnInit(): void {}
+
+  navigateTo(url:string){
+    this.router.navigate([url])
+  }
+
+
+  toggle() {
+    this.sidebarService.toggle(true);
+    return false;
+  }
+  items: NbMenuItem[] = [
+    {
+      title: 'Profile',
+      badge: {
+        text: '3',
+        status: 'primary',
+      },
+      children: [
+        {
+          title: 'Messages',
+          badge: {
+            status: 'danger',
+          },
+        },
+        {
+          title: 'Notifications',
+          badge: {
+            dotMode: true,
+            status: 'warning',
+          },
+        },
+        {
+          title: 'Emails',
+          badge: {
+            text: 'new',
+            status: 'success',
+          },
+        },
+      ],
+    },
+  ];
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule,
+         NbLayoutModule, 
+         NbSidebarModule,
+         NbThemeService,
+         NbMenuModule,
+} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+
+import { FormsModule } from '@angular/forms';
+import { HomeModule } from './pages/homes/home/home.module';
+import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +22,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    FormsModule,
+    HomeModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbSidebarModule.forRoot(),
+    NbMenuModule.forRoot(),
+    NbEvaIconsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [NbThemeService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
+
+
